a175518a567f170b054640c34b95873d0438e4ed
pawelduda <pawelduda91@gmail.com>
2016-07-21 23:00:17 +0200

    Change KV.Bucket.delete/2 to act inside client rather than server process

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -24,6 +24,8 @@ defmodule KV.Bucket do
   Deletes a `key` from the `bucket` and returns its `value`.
   """
   def delete(bucket, key) do
-    Agent.get_and_update(bucket, &Map.pop(&1, key))
+    Agent.get_and_update(bucket, fn(dict) ->
+      Map.pop(dict, key)
+    end)
   end
 end
