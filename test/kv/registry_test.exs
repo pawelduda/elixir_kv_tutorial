b461934b14a9c684e7a8064388b1875c11516365
pawelduda <pawelduda91@gmail.com>
2016-07-23 00:23:05 +0200

    Add process monitoring to registry in order to prevent serving stale entries

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -16,4 +16,12 @@ defmodule KV.RegistryTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
