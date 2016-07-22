74dfcbf81ae286623887191752fbf46144b9ecfa
pawelduda <pawelduda91@gmail.com>
2016-07-23 01:24:28 +0200

    Implement supervisor tree

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -2,8 +2,8 @@ defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
   doctest KV.Registry
 
-  setup do
-    {:ok, registry} = KV.Registry.start_link
+  setup context do
+    {:ok, registry} = KV.Registry.start_link(context.test)
     {:ok, registry: registry}
   end
 
@@ -22,6 +22,20 @@ defmodule KV.RegistryTest do
     assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "removes buckets on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with a non-normal reason
+    Process.exit(bucket, :shutdown)
+
+    # Wait until the bucket is dead
+    ref = Process.monitor(bucket)
+    assert_receive {:DOWN, ^ref, _, _, _}
+
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
